9281b1d11ce6b8ac793c77f9ed3f322b5fc518ed
abhijitchaudhary28 <abhijit.chaudhary.cs28@iilm.edu>
2026-04-12 17:03:28 +0530

    Implement searchMatrix for 2D matrix search

---
+++ PALB/Search in a 2D/code.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        if not matrix or not matrix[0]:
+            return False
+        rows = len(matrix)
+        cols = len(matrix[0])
+        low = 0
+        high = rows * cols - 1
+        while low <= high:
+            mid = (low + high) // 2
+            mid_val = matrix[mid // cols][mid % cols]
+            if mid_val == target:
+                return True
+            elif mid_val < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+                
+        return False
